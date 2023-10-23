3a9c18fc9947874f5f6a7fdc4782e2131239505c
Coderbutgs <gauravkumar9303@gmail.com>
2023-10-23 18:21:08 +0530

    Added validation and authentication for user login.

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoURI = "mongodb://0.0.0.0:27017/";
+const mongoURI = "mongodb://0.0.0.0:27017/Medico";
 
 const connectToMongo = async ()=>{
     mongoose.connect(mongoURI,await 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const User = require("../Models/User");
+const {body,validationResult} = require("express-validator");
 const router = express.Router();
 
-router.post('/',(req,res)=>{
-    res.send("Hello");
-    const user = User(req.body);
-    console.log(req.body);
-    user.save();
+router.post('/',[
+    body('email','Enter a valid email').isEmail(),
+    body('firstName',"Enter a valid first name").isLength({min:3}),
+    body('middleName',"Enter a valid middle name").isLength({min:3}),
+    body('lastName',"Enter a valid last name").isLength({min:3}),
+    body('contact',"Enter a valid 10 digit phone number").isisLength(10),
+    body('password',"Enter a valid password").isLength({min:5}),
+],(req,res)=>{
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        res.send({ errors: errors.array() });
+    }
+    User.create(
+        {
+            firstName:req.body.firstName,
+            middleName:req.body.middleName,
+            lastName:req.body.lastName,
+            email:req.body.email,
+            contact:req.body.contact,
+            password:req.body.password
+        }
+    ).then(user => res.json(user))
+    .catch(err=>{
+    res.json({error:"Please enter a unique value for email",message:err.message})
+    })
 })
 
 module.exports = router;
