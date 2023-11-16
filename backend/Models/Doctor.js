0ba1b4e4fc3cdf65ce80f0be92634161d54194c3
Coderbutgs <gauravkumar9303@gmail.com>
2023-11-16 16:35:14 +0530

    made a doctor model

---
+++ backend/Models/Doctor.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const DoctorSchema = new Schema({
+    firstName:{
+        type:String,
+        required : true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    specialization:{
+        type:String,
+        required:true
+    },
+    hospital:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'hospital'
+    },
+    experience:{
+        type:Number,
+        required:true
+    },
+    photo:{
+    
+    }
+})
+
+const Doctor = mongoose.model('doctor',DoctorSchema);
+
+module.exports= Doctor;
