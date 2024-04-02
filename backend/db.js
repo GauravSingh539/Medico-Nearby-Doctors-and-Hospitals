0a2ae12c818a22ae77f8c627ea7a39a86a9dcc7a
Coderbutgs <gauravkumar9303@gmail.com>
2024-04-02 12:38:38 +0530

    //Changed the db path again to compass

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-//mongodb://0.0.0.0:27017/Medico
-const mongoURI = "mongodb+srv://Coderbutgs:Gaurav@9303@medico.ouetrsn.mongodb.net/?retryWrites=true&w=majority&appName=Medico";
+
+
+const mongoURI = "mongodb://0.0.0.0:27017/Medico";
 
 const connectToMongo = async ()=>{
     mongoose.connect(mongoURI,await 
@@ -9,3 +10,6 @@ const connectToMongo = async ()=>{
 }
 
 module.exports = connectToMongo;
+
+
+
